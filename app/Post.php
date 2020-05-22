737e9593d341cf2e487b4404a1ef1ed206f2ddbd
Wagner Junior <wagner.junior30@gmail.com>
2020-05-22 12:30:06 -0300

    :rocket: Added GraphQL types and queries

--- app/Post.php
+++ app/Post.php
@@ -3,16 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     //
-    public function user()
+    public function user() : BelongsTo
     {
         return $this->belongsTo('App\User');
     }
 
-    public function comments()
+    public function comments() : HasMany
     {
         return $this->hasMany('App\Comment');
     }
